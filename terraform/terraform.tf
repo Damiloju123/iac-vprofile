0bcd84222e4c301be01465f21fe435b6be9fda8a
damiloju <jdamiloju@gmail.com>
2025-10-12 17:51:38 +0100

    AWS Version fix

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"   # compatible with EKS module v21.0.0
+      version = "~> 6.0"   # compatible with EKS module v21.0.0
     }
 
     random = {
