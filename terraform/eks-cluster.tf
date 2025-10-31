b409c1e8e1930f7a2b9d058e2b34b33a289a6d37
damiloju <jdamiloju@gmail.com>
2025-10-31 14:43:30 +0000

    fmt fix

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,11 +5,12 @@ module "eks" {
   name               = local.cluster_name
   kubernetes_version = "1.31"
 
-  vpc_id                      = module.vpc.vpc_id
-  subnet_ids                  = module.vpc.private_subnets
-  endpoint_public_access      = true
-  endpoint_private_access     = true
-  enable_irsa = true
+  vpc_id                       = module.vpc.vpc_id
+  subnet_ids                   = module.vpc.private_subnets
+  endpoint_public_access       = true
+  endpoint_private_access      = true
+  endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  enable_irsa                  = true
 
   eks_managed_node_groups = {
     one = {
