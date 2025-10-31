84897960b5f7994c1ec181094b41c51d62b65a7a
damiloju <jdamiloju@gmail.com>
2025-10-31 14:29:32 +0000

    FIX

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,13 +1,17 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.3.2"
+  version = "~> 21.0"
 
   name               = local.cluster_name
-  kubernetes_version = "1.32"
+  kubernetes_version = "1.31"
 
-  vpc_id                 = module.vpc.vpc_id
-  subnet_ids             = module.vpc.private_subnets
-  endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  endpoint_public_access         = true
+  endpoint_private_access        = true
+  endpoint_public_access_cidrs   = ["0.0.0.0/0"]
+
+  enable_irsa = true
 
   eks_managed_node_groups = {
     one = {
@@ -18,6 +22,8 @@ module "eks" {
       min_size     = 1
       max_size     = 3
       desired_size = 2
+
+      subnet_ids = module.vpc.private_subnets
     }
 
     two = {
@@ -28,6 +34,8 @@ module "eks" {
       min_size     = 1
       max_size     = 2
       desired_size = 1
+
+      subnet_ids = module.vpc.private_subnets
     }
   }
 }
